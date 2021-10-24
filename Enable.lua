475f5afed530cc39eaa84a4b1b6b76e29df996d8
Ethan <61071128+Ethanoj1@users.noreply.github.com>
2021-10-23 19:55:27 -0700

    hour of discontinuation

--- Enable.lua
+++ Enable.lua
@@ -3,8 +3,8 @@ if not _G.Discontinuation then
   game.StarterGui:SetCore("SendNotification", {
       Title = "Better Script";
       Text = "This script is old and outdated, use Eclipse Hub for a faster autofarm (In Discord)!";
-      Duration = 15;
+      Duration = 3600;
   }) 
   _G.Discontinuation = true
 end
---So many skids taking credit for my work lmao
+--Switch to Eclipse Hub, Eclipse MM2 is discontinued
