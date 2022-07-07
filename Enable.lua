6c1a94f87e42b99b2df823d04ce8e225e3bdf691
Ethan <61071128+Ethanoj1@users.noreply.github.com>
2022-07-07 02:09:18 -0700

    Reaching EOL

--- Enable.lua
+++ Enable.lua
@@ -1,18 +1,19 @@
 print("Checking")
 if not _G.Discontinuation then
-  local bind = Instance.new("BindableFunction")
+  --[[local bind = Instance.new("BindableFunction")
   bind.OnInvoke = function(buttonText)
       if buttonText == "Join Discord" then
           _G.Join()
       end
-  end
+  end]]
   game.StarterGui:SetCore("SendNotification", {
       Title = "Better Script";
       Text = "This script is old and outdated, use Eclipse Hub for a faster autofarm (In Discord)!";
       Duration = 360000;
-      Button1 = "Join Discord";
-      Callback = bind;
+      --Button1 = "Join Discord";
+      --Callback = bind;
   }) 
+  _G.Join()
   _G.Discontinuation = true
 end
 --Switch to Eclipse Hub, Eclipse MM2 is discontinued
